3c3311c677829fa25cfb0cfd3a2c63e21610d390
= <=>
2022-07-12 17:00:50 -0400

    reconfigured setup and teardown files for jest

--- server/server.js
+++ server/server.js
@@ -57,8 +57,6 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(PORT, () => {
+module.exports = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
 });
-
-// module.exports = app;
